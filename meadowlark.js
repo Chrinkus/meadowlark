37bf4fb7997c860eed11f4ffebbc37ba20040e55
Chrinkus <seeschickrun@gmail.com>
2017-07-03 00:48:29 -0500

    Begin global and page testing

--- meadowlark.js
+++ meadowlark.js
@@ -12,13 +12,23 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(express.static(__dirname + "/public"));
 
+// Mocha testing
+app.use((req, res, next) => {
+    res.locals.showTests = app.get("env") !== "production" &&
+            req.query.test === "1";
+    next();
+});
+
 // Routing
 app.get("/", (req, res) => {
     res.render("home");
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", { fortune: fortune.getFortune() });
+    res.render("about", {
+        fortune: fortune.getFortune(),
+        pageTestScript: "/qa/tests-about.js"
+    });
 });
 
 // custom 404 page
---
+++ public/qa/tests-global.js
@@ -0,0 +1,6 @@
+suite("Global Tests", () => {
+    test("page has valid title", () => {
+        assert(document.title && document.title.match(/\S/) &&
+                document.title.toUpperCase() !== "TODO");
+    });
+});
